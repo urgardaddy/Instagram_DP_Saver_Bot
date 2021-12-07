13cb7e407227f4608f6a3bee427da4bfef1331bc
Sugar daddy <94607538+urgardaddy@users.noreply.github.com>
2021-12-07 08:25:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ def username(update, context):
 
 
 def source(update, context):
-    update.message.reply_text("You can get the source code of this bot here \n\n https://github.com/anishgowda21/Instagram_DP_Saver_Bot")
+    update.message.reply_text("You can get the source code of this bot here \n\n https://github.com")
 
 
 def error(update, context):
